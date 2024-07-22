7ea741edae27a81c0648aba0675fdee11caaf4cb
AvneetKaur9997 <av4369997@alphacollege.me>
2024-07-22 23:48:11 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def respond(
     temperature,
     top_p,
 ):
-    system_message = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise."
+    system_message ="You're exploring Indian dance forms. I can guide you through styles like Bharatanatyam's graceful movements, Kathak's storytelling, or Odissi's fluidity. What interests you most?"
     messages = [{"role": "system", "content": system_message}]
 
     for val in history:
@@ -46,7 +46,7 @@ For information on how to customize the ChatInterface, peruse the gradio docs: h
 demo = gr.ChatInterface(
     respond,
     additional_inputs=[
-        gr.Textbox(value = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise.", label="System message"),
+        gr.Textbox(value = "You're exploring Indian dance forms. I can guide you through styles like Bharatanatyam's graceful movements, Kathak's storytelling, or Odissi's fluidity. What interests you most?", label="System message"),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
         gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
         gr.Slider(
@@ -59,11 +59,11 @@ demo = gr.ChatInterface(
     ],
 
     examples = [ 
-        ["I feel overwhelmed with work."],
-        ["Can you guide me through a quick meditation?"],
-        ["How do I stop worrying about things I can't control?"]
+        ["Which Indian dance style emphasizes storytelling?"],
+        ["Can you recommend a beginner-friendly Indian dance style?"],
+        ["What is the significance of mudras in classical Indian dance?"]
     ],
-    title = 'Calm Mate 🕊️'
+    title = 'Indian Dance Form'
 )
 
 
